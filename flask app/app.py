051d8dec7a6fe9c3e5d05f86dc13948a3139a109
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-07-16 19:39:44 -0500

    Co-authored-by: Ryan Lowe <icyfrostbolt@users.noreply.github.com> Co-authored-by: Emmanuel Taiwo <T4910@users.noreply.github.com> Co-authored-by: Awesome-Rex <Awesome-Rex@users.noreply.github.com>

--- flask app/app.py
+++ flask app/app.py
@@ -1,34 +1,50 @@
-from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
+from Flask import Flask, flash, redirect, render_template, request, session
+from Flask_Session import Session
+from Werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
 
 
 app = Flask(__name__)
 
-# # ensures users are logged in first
-# def login_required(f):
-#     """
-#     Decorate routes to require login.
+Session(app)
 
-#     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-#     """
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         if session.get("user_id") is None: 
-#             return redirect("/login")
-#         return f(*args, **kwargs)
-#     return decorated_function
+# ensures users are logged in first
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None: 
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route("/")
+@login_required
 def index():
     return render_template('mainpage.html')
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
+
+        name = request.form.get('username')
+        password = request.form.get('password')
+
+        if name == 'go' and password == 'pas':
+            return redirect('/')
+
         return render_template('mainpage.html')
+
+    # GET
     else:
-        return render_template("404.html")
+        return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
